17600ab9d3823f6ce95113fcdcf82b23470e6d7a
Евгений Ламбов < >
2015-11-27 19:08:23 +0200

    js

---
+++ GoIT/javascript/home/lesson1/js/js/js/script.js
@@ -0,0 +1,12 @@
+alert('Бро, хочешь котэ увидеть? Веди логин и пароль который я тебе дал')
+// login
+var user;
+user = prompt('Ваш логин');
+if (user == 'Андрюха');
+// password
+var password;
+password = prompt('Ваш пароль');
+if (password == '1234'){
+	alert('Молодец! Жми ок и увидешь Котэ)');
+}
+

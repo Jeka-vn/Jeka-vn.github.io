7e813e59255943f091aa26f3096e847b372eec67
Евгений Ламбов < >
2015-12-06 18:42:00 +0200

    ad dz 11

--- GoIT/javascript/home/lesson1/DZ_11/js/script.js
+++ GoIT/javascript/home/lesson1/DZ_11/js/script.js
@@ -1,14 +1,17 @@
-// записали
 var a = [];
-var b = [];
-for (var i = 0; i < 2; i++) {
+for (var i = 0; i < 5; i++) { 
 	a.push(prompt('Введите имя'));
 };
-// читаем
-b.push(prompt('Имя'));
-if (a == b) {
-  document.write( 'Да вы знаток!' );
+var b = prompt('Ваше имя');
+var flag;
+for (i = 0; i < a.length; i++) {
+	if (a[i] === b) {
+		flag = true;
+	}
+};
+if (flag == true) {
+	alert(b + ',' + ' Поздровляю! Вы успешно вошли.');
 } else {
-  alert( 'А вот и неправильно!' );
+	alert('Имя пользователя не верное.')
 }
 

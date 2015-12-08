5dd3ea77088c39167536b7d44fe5e600aa67ae74
Евгений Ламбов < >
2015-12-08 16:34:12 +0200

    dz

---
+++ GoIT/javascript/home/lesson1/DZ_12/js/script.js
@@ -0,0 +1,73 @@
+var element = document.createElement('div');
+element.classList.add('wrapper');
+var wrapper = document.querySelector('body');
+wrapper.appendChild(element);
+var element = document.createElement('p');
+element.classList.add('h1');
+element.innerHTML = 'Тест по програмированию';
+var wrapper = document.querySelector('.wrapper');
+wrapper.appendChild(element);
+
+var element = document.createElement('div');
+element.classList.add('headers');
+var wrapper = document.querySelector('.wrapper');
+wrapper.appendChild(element);
+
+var element = document.createElement('p');
+element.classList.add('box');
+element.innerHTML = '1. Задание №1';
+var wrapper = document.querySelector('.headers');
+wrapper.appendChild(element);
+
+var element = document.createElement('input');
+element.setAttribute("type", "radio");
+var wrapper = document.querySelector('.headers');
+wrapper.appendChild(element);
+
+var element = document.createElement('strong');
+element.innerHTML = ' 1. Задание №1';
+var wrapper = document.querySelector('.headers');
+wrapper.appendChild(element);
+
+
+var element = document.createElement('input');
+element.setAttribute("type", "radio");
+var wrapper = document.querySelector('.headers');
+wrapper.appendChild(element);
+
+var element = document.createElement('strong');
+element.innerHTML = ' 1. Задание №1';
+var wrapper = document.querySelector('.headers');
+wrapper.appendChild(element);
+
+
+
+
+
+
+
+
+// var element = document.createElement('div');
+// element.classList.add('chec');
+// var wrapper = document.querySelector('.wrapper');
+// wrapper.appendChild(element);
+// var element = document.createElement('div');
+// element.classList.add('chec1');
+// var wrapper = document.querySelector('.wrapper');
+// wrapper.appendChild(element);
+// var element = document.createElement('div');
+// element.classList.add('chec2');
+// var wrapper = document.querySelector('.wrapper');
+// wrapper.appendChild(element);
+// var element = document.createElement('input');
+// element.setAttribute("type", "radio");
+// var wrapper = document.querySelector('.chec');
+// wrapper.appendChild(element);
+// var element = document.createElement('input');
+// element.setAttribute("type", "radio");
+// var wrapper = document.querySelector('.chec1');
+// wrapper.appendChild(element);
+// var element = document.createElement('input');
+// element.setAttribute("type", "radio");
+// var wrapper = document.querySelector('.chec2');
+// wrapper.appendChild(element);

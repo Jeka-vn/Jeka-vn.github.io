b49f093dd22150b1e766c6a52ee261118f099b57
Евгений Ламбов < >
2015-12-13 15:01:15 +0200

    ds12

--- GoIT/javascript/home/lesson1/DZ_12/js/script.js
+++ GoIT/javascript/home/lesson1/DZ_12/js/script.js
@@ -1,73 +1,53 @@
-var element = document.createElement('div');
-element.classList.add('wrapper');
-var wrapper = document.querySelector('body');
-wrapper.appendChild(element);
-var element = document.createElement('p');
-element.classList.add('h1');
-element.innerHTML = 'Тест по програмированию';
+var app = {
+	createElement: function(params) {
+		var element = document.createElement(params.tagName);
+		if (params.inputType) {
+			element.setAttribute('type', params.inputType);
+		}
+		element.className = params.className;
+		element.innerHTML = params.content;
+		if (params.parentElement) {
+			params.parentElement.appendChild(element);
+		}
+		return element;
+	},
+	generateQuestion: function(questionsAmount, answersAmount) {
+		for (var i = 0; i < questionsAmount; i++) {
+			this.createElement({
+				tagName: 'h2',
+				content: 'Вопрос №' + (i + 1),
+				parentElement: wrapper
+			});
+			for (var j = 0; j < answersAmount; j++)
+				this.createElement({
+					tagName: 'p',
+					content: 'Вариант ответа №' + (i + 1),
+					parentElement: wrapper
+				});
+		}
+	}
+}
+var body = document.querySelector('body');
+app.createElement({
+	tagName: 'div',
+	className: 'wrapper',
+	content: '',
+	parentElement: body
+});
 var wrapper = document.querySelector('.wrapper');
-wrapper.appendChild(element);
-
-var element = document.createElement('div');
-element.classList.add('headers');
-var wrapper = document.querySelector('.wrapper');
-wrapper.appendChild(element);
-
-var element = document.createElement('p');
-element.classList.add('box');
-element.innerHTML = '1. Задание №1';
-var wrapper = document.querySelector('.headers');
-wrapper.appendChild(element);
-
-var element = document.createElement('input');
-element.setAttribute("type", "radio");
-var wrapper = document.querySelector('.headers');
-wrapper.appendChild(element);
-
-var element = document.createElement('strong');
-element.innerHTML = ' 1. Задание №1';
-var wrapper = document.querySelector('.headers');
-wrapper.appendChild(element);
-
-
-var element = document.createElement('input');
-element.setAttribute("type", "radio");
-var wrapper = document.querySelector('.headers');
-wrapper.appendChild(element);
-
-var element = document.createElement('strong');
-element.innerHTML = ' 1. Задание №1';
-var wrapper = document.querySelector('.headers');
-wrapper.appendChild(element);
+app.createElement({
+	tagName: 'h1',
+	className: 'title',
+	content: 'Тест по програмированю',
+	parentElement: wrapper
+});
+app.generateQuestion(5, 3);
+app.createElement({
+	tagName: 'input',
+	className: 'chec',
+	inputType: 'submit',
+	parentElement: wrapper
+});
 
 
 
-
-
-
-
-
-// var element = document.createElement('div');
-// element.classList.add('chec');
-// var wrapper = document.querySelector('.wrapper');
-// wrapper.appendChild(element);
-// var element = document.createElement('div');
-// element.classList.add('chec1');
-// var wrapper = document.querySelector('.wrapper');
-// wrapper.appendChild(element);
-// var element = document.createElement('div');
-// element.classList.add('chec2');
-// var wrapper = document.querySelector('.wrapper');
-// wrapper.appendChild(element);
-// var element = document.createElement('input');
-// element.setAttribute("type", "radio");
-// var wrapper = document.querySelector('.chec');
-// wrapper.appendChild(element);
-// var element = document.createElement('input');
-// element.setAttribute("type", "radio");
-// var wrapper = document.querySelector('.chec1');
-// wrapper.appendChild(element);
-// var element = document.createElement('input');
-// element.setAttribute("type", "radio");
-// var wrapper = document.querySelector('.chec2');
-// wrapper.appendChild(element);

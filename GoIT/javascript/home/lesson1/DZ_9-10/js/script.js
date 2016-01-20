1d4cfc5fbeed94572faaf4a3f83caa0c80013d8f
Евгений Ламбов < >
2016-01-20 15:24:59 +0200

    dz 9-10

--- GoIT/javascript/home/lesson1/DZ_9-10/js/script.js
+++ GoIT/javascript/home/lesson1/DZ_9-10/js/script.js
@@ -1,21 +1,23 @@
 $(function () {
-	var $links = $('.punkt li');
-	$links.on('click', function () {
-		var $subMenu = $(this).siblings('.sub');
-		$subMenu.toggle(150);
+	$('.test, .submenu').on('click', function (e) {
+		$('.submenu').toggle();
+		e.preventDefault();
 	});
+$('.test1, .submenu1').hover(function (e) {
+		$('.submenu1').toggle();
+		e.preventDefault();
 });
 
-$(function () {
 	var $images = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg'];
 	var $curectIMG = 0;
 	$('#img').attr('src', $images[$curectIMG]);
-	$('.left__a').on('click', function () {
+	$('.left__a').on('click', function (e) {
 		if ($curectIMG != 0) {
 			$curectIMG--;
 			$('#img').fadeToggle('fast');
 			$('#img').fadeToggle('fast');
 			$('#img').attr('src', $images[$curectIMG]);
+		e.preventDefault();
 		}
 	});
 	$('.rigth__a').on('click', function () {
@@ -26,4 +28,4 @@ $(function () {
 			$('#img').attr('src', $images[$curectIMG]);
 		}
 	});
-})
+});

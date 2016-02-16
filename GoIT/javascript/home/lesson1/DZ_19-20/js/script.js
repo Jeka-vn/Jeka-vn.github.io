6314035ec2bf43bcd879c1ff4a54a8804d0ee7bb
Евгений Ламбов < >
2016-02-16 14:55:21 +0200

    ad dz 19-20 25%

--- GoIT/javascript/home/lesson1/DZ_19-20/js/script.js
+++ GoIT/javascript/home/lesson1/DZ_19-20/js/script.js
@@ -1,3 +1,102 @@
 $(function() {
+	var click = 0,
+	click1 = 0,
+	click2 = 0,
+	click3 = 0;
+	$('.benners__panel').on('click', function() {
+		$('.panel__content').slideToggle();
+		$('.benners__panel').css({
+			background: '#f4b60d'
+		})
+		$('.benners__panel span').css({
+			color: '#fff'
+		})
+		$('.panel__box img').attr('src', 'img/panel/minus.png');
+		click += 1;
+		if (click === 2) {
+			$('.benners__panel').css({
+				background: '#fff'
+			})
+			$('.benners__panel span').css({
+				color: '#000'
+			})
+			$('.panel__box img').attr('src', 'img/panel/plus.png');
+		}
+		if (click === 3) {
+			click -= 2;
+		}
+	})
+
+	$('.benners__panel1').on('click', function() {
+		$('.panel__content1').slideToggle();
+		$('.benners__panel1').css({
+			background: '#f4b60d'
+		})
+		$('.benners__panel1 span').css({
+			color: '#fff'
+		})
+		$('.panel__box1 img').attr('src', 'img/panel/minus.png');
+		click1 += 1;
+		if (click1 === 2) {
+			$('.benners__panel1').css({
+				background: '#fff'
+			})
+			$('.benners__panel1 span').css({
+				color: '#000'
+			})
+			$('.panel__box1 img').attr('src', 'img/panel/plus.png');
+		}
+		if (click1 === 3) {
+			click1 -= 2;
+		}
+	})
+
+	$('.benners__panel2').on('click', function() {
+		$('.panel__content2').slideToggle();
+		$('.benners__panel2').css({
+			background: '#f4b60d'
+		})
+		$('.benners__panel2 span').css({
+			color: '#fff'
+		})
+		$('.panel__box2 img').attr('src', 'img/panel/minus.png');
+		click2 += 1;
+		if (click2 === 2) {
+			$('.benners__panel2').css({
+				background: '#fff'
+			})
+			$('.benners__panel2 span').css({
+				color: '#000'
+			})
+			$('.panel__box2 img').attr('src', 'img/panel/plus.png');
+		}
+		if (click2 === 3) {
+			click2 -= 2;
+		}
+	})
+
+	$('.benners__panel3').on('click', function() {
+		$('.panel__content3').slideToggle();
+		$('.benners__panel3').css({
+			background: '#f4b60d'
+		})
+		$('.benners__panel3 span').css({
+			color: '#fff'
+		})
+		$('.panel__box3 img').attr('src', 'img/panel/minus.png');
+		click3 += 1;
+		if (click3 === 2) {
+			$('.benners__panel3').css({
+				background: '#fff'
+			})
+			$('.benners__panel3 span').css({
+				color: '#000'
+			})
+			$('.panel__box3 img').attr('src', 'img/panel/plus.png');
+		}
+		if (click3 === 3) {
+			click3 -= 2;
+		}
+	})
 
 });

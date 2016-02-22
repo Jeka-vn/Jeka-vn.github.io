eec5697e50904c7f093facc437bb62bc002aa380
Евгений Ламбов < >
2016-02-22 22:47:53 +0200

    ad modus 0%

---
+++ homework/html/MODUS_versus/gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+ 
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    notify = require("gulp-notify");
+ 
+gulp.task('sass', function () {
+  return gulp.src('./app/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(notify("Done!"));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('./app/scss/style.scss', ['sass']);
+});

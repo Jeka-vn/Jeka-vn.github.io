2c2de31c4c3b30a6675ac5ddc9950c70d8bf4715
Евгений Ламбов < >
2016-03-01 13:25:40 +0200

    ad dz 23-24 99.5%

---
+++ homework/JavaScript/JS/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// var arr = ['Lorem ipsum.', 'Lorem ipsum dolor.', 'Lorem ipsum.', 'Lorem ipsum dolor sit.', 'Lorem ipsum dolor sit.', 'Lorem ipsum dolor sit.', 'Lorem ipsum dolor sit amet, consectetur.', 'Lorem ipsum dolor sit.'];
+
+var name = prompt('Введите Ваше имя');
+
+function greeting(a, b) {
+	console.log(a + b);
+}
+greeting('Hello ', name);

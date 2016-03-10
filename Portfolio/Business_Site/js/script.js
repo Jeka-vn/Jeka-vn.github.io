689da7fb9e4079ac75c26e70a631c5b140f99ce6
Евгений Ламбов < >
2016-03-10 15:38:35 +0200

    ad bisness site 90&%

--- Portfolio/Business_Site/js/script.js
+++ Portfolio/Business_Site/js/script.js
@@ -1,6 +1,23 @@
 $(function () {
 
+	$('.radio_in').on('click', function () {
+
+		var radioIndex = $(this).index();
+
+		if (radioIndex === radioIndex) {
+			$('#slider_img').attr({
+				src: 'img/siteImg/slider/'+radioIndex+'.jpg'
+			})
+		}
+	});
+
+$('.what_menu .what_link').on('click', function (e) {
+var linkIndex = $('.what_link').index();
+console.log(linkIndex);
 
 
+	e.preventDefault();
+})
 
 });
+

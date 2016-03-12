638e668086310269d64fde7a8ee54c9345328387
Евгений Ламбов < >
2016-03-12 22:40:41 +0200

    da 25-26 20%

--- GoIT/javascript/home/lesson1/DZ_25-26/js/script.js
+++ GoIT/javascript/home/lesson1/DZ_25-26/js/script.js
@@ -1,11 +1,44 @@
-$(function() {
-	
 
-$('.activity').masonry({
-  // options
-  itemSelector: '.item',
-  columnWidth: 1
-});
+$(function() {
+
+	$('.activity').masonry({
+		itemSelector: '.item',
+		columnWidth: 1
+	});
+	
+	$.ajax({
+		url: 'http://api.pixplorer.co.uk/image?word=sea&amount=7&size=600',
+		type: 'get',
+		dataType: "JSON",
+		success: function(data){
+			for (var i = 0; i < 7; i++) {
+				var $data = data.images[i].imageurl;
+				var $title = data.images[i].word;
+				$('.img'+i+'').attr('src', $data);
+				$('.title'+i+'').html($title);
+			}
+		}
+	});
+
+
+	$('.ideas__submit').on('click', function (e) {
+		var $input = $('.ideas__search').val();
+
+		$.ajax({
+			url: 'http://api.pixplorer.co.uk/image?word='+ $input +'&amount=7&size=tb',
+			type: 'get',
+			dataType: "JSON",
+			success: function(data){
+				for (var i = 0; i < 7; i++) {
+					var $data = data.images[i].imageurl;
+					var $title = data.images[i].word;
+					$('.img'+i+'').attr('src', $data);
+					$('.title'+i+'').html($title);
+				}
+			}
+		});
+		e.preventDefault();
+	})
 
 
 });

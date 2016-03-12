e7be3b8d7fb0f1f9f44ccc11f20568e74d49ecf3
Евгений Ламбов < >
2016-03-12 23:29:09 +0200

    dz 25-26 21%

--- GoIT/javascript/home/lesson1/DZ_25-26/js/script.js
+++ GoIT/javascript/home/lesson1/DZ_25-26/js/script.js
@@ -1,44 +1,74 @@
 
 $(function() {
+// Slider
+var $colection = ['img/siteImg/works/st1.jpg', 'img/siteImg/works/st2.jpg', 'img/siteImg/works/st3.jpg', 'img/siteImg/works/4.jpg', 'img/siteImg/works/5.jpg', 'img/siteImg/works/6.jpg', 'img/siteImg/works/7.jpg', 'img/siteImg/works/8.jpg', 'img/siteImg/works/9.jpg'];
 
-	$('.activity').masonry({
-		itemSelector: '.item',
-		columnWidth: 1
-	});
-	
+
+for (var i = 0; i < $colection.length; i++) {
+	console.log($colection[i]);
+}
+
+
+$('.works__left').on('click', function () {
+	$('.works__img').attr('src', $colection[3])
+})
+
+
+
+
+
+
+
+
+$('.works__left, .works__right').on('click', function (e) {
+	e.preventDefault();
+});
+
+// Galery
+$('.activity').masonry({
+	itemSelector: '.item',
+	columnWidth: 1
+});
+
+$.ajax({
+	url: 'http://api.pixplorer.co.uk/image?word=sea&amount=7&size=600',
+	type: 'get',
+	dataType: "JSON",
+	success: function(data){
+
+		for (var i = 0; i < 7; i++) {
+			var $data = data.images[i].imageurl,
+			$title = data.images[i].word;
+			$('.img'+i+'').attr('src', $data);
+			$('.title'+i+'').html($title);
+		}
+
+	}
+
+});
+
+
+$('.ideas__submit').on('click', function (e) {
+	var $input = $('.ideas__search').val();
+
 	$.ajax({
-		url: 'http://api.pixplorer.co.uk/image?word=sea&amount=7&size=600',
+		url: 'http://api.pixplorer.co.uk/image?word='+ $input +'&amount=7&size=tb',
 		type: 'get',
 		dataType: "JSON",
 		success: function(data){
+
 			for (var i = 0; i < 7; i++) {
-				var $data = data.images[i].imageurl;
-				var $title = data.images[i].word;
+				var $data = data.images[i].imageurl,
+				$title = data.images[i].word;
 				$('.img'+i+'').attr('src', $data);
 				$('.title'+i+'').html($title);
 			}
+
 		}
+
 	});
-
-
-	$('.ideas__submit').on('click', function (e) {
-		var $input = $('.ideas__search').val();
-
-		$.ajax({
-			url: 'http://api.pixplorer.co.uk/image?word='+ $input +'&amount=7&size=tb',
-			type: 'get',
-			dataType: "JSON",
-			success: function(data){
-				for (var i = 0; i < 7; i++) {
-					var $data = data.images[i].imageurl;
-					var $title = data.images[i].word;
-					$('.img'+i+'').attr('src', $data);
-					$('.title'+i+'').html($title);
-				}
-			}
-		});
-		e.preventDefault();
-	})
+	e.preventDefault();
+});
 
 
 });

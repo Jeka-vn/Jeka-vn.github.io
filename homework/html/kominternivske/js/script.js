7619b0f12a83d319fc6f0a0a8a1286695c69c33e
Евгений Ламбов < >
2016-04-25 22:30:52 +0300

    del kominternivske

--- homework/html/kominternivske/js/script.js
+++ homework/html/kominternivske/js/script.js
@@ -7,4 +7,24 @@ $(function () {
 		})
 	})();
 
+	//valid form comment
+
+	(function valid() {
+		var $error = {
+			name: 'Будь ласка, заповниіть поле "Ваше Ім\'\я"!',
+			email: 'Ви не вірно заповнили поле "Ваш E-mail"'
+		};
+
+		$('.form__submit').on('click', function (e) {
+			var $formName = $('.form__name').val().length;
+
+			if ($formName === 0) {
+				$('.error__message').show(200).html($error.name);
+			} else {
+				$('.error__message').hide(200);
+			}
+			e.preventDefault();
+		})
+	})();
+
 });

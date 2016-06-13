f33067a732368a53a2f39d43bcf9742e1a7d838b
Евгений Ламбов < >
2016-06-13 13:09:20 +0300

    ad carft 20%

---
+++ Portfolio/CARFT_LANCE/gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+// variables
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require("gulp-rename");
+
+// task
+		gulp.task('scss', function() {
+			return gulp.src('./app/**/**/*.scss')
+			.pipe(sass().on('error', sass.logError))
+			// .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+			.pipe(gulp.dest('./css'));
+		});
+
+		gulp.task('rename', function() {
+			gulp.src('css/**/*.css')
+			.pipe(rename('style.css'))
+			.pipe(gulp.dest("./css/"));
+		});
+
+//  watch
+
+		gulp.task('default', function() {
+			gulp.watch('./app/**/**/*.scss', ['scss']);
+			gulp.watch('./css/**/*.css', ['rename']);
+		});

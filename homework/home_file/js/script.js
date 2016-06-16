c51bfd0ff68388d9d112346d3c90b699ce669027
Евгений Ламбов < >
2016-06-16 13:48:00 +0300

    ad slider

--- homework/home_file/js/script.js
+++ homework/home_file/js/script.js
@@ -1,10 +1,31 @@
 $(function() {
 
-	function showLogo() {
-		$('body').animate({
-			opacity: '1'
-		}, 1000)
-	}
-	setTimeout(showLogo, 100);
+	(function preview() {
+
+		$('.contentHover > i').on('click', function () {
+			var $index = $('.contentHover > i').index(this);
+			var $contentImgAttr = $('.contentImg').eq($index).attr('data');
+
+			(function showImg() {
+				$('.show').css({
+					'display': 'block'
+				}).animate({
+					'opacity': '1'
+				}, 500)
+				$('.show').append('<span class="fa fa-times fa-2x" aria-hidden="true"></span>');
+				$('.show').append('<div class="showFullImgBox">');
+				$('.showFullImgBox').append('<img class="showFullImg">');
+				$('.showFullImg').attr('src', $contentImgAttr);
+
+				$('.show span').on('click', function () {
+					$('.show').css({
+						'display': 'none',
+						'opacity': '0'
+					})
+					$('.showFullImgBox, .show span').remove();
+				})
+			})();
+		})
+	})();
 	
 });
